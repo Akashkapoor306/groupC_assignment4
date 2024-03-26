d0386c28b3344b75a62900e68410a5fa1a042567
AkashKapoor <akashkapoor306@gmail.com>
2024-03-26 19:44:51 -0400

    Fetch Weather data from External Api

--- main.go
+++ main.go
@@ -1,5 +1,12 @@
 package main
 
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"strings"
+)
+
 // ExternalAPIResponse defines the structure for the external weather API response
 type ExternalAPIResponse struct {
 	Temperature string `json:"temperature"`
@@ -17,4 +24,25 @@ type WeatherResponse struct {
 	City        string `json:"city"`
 	Temperature string `json:"temperature"`
 	Weather     string `json:"weather"` // We'll use the "Description" from the external API here
+}
+
+// getWeather fetches weather data for a city using the external API
+func getWeather(cityName string) (WeatherResponse, error) {
+	url := fmt.Sprintf("https://goweather.herokuapp.com/weather/%s", cityName)
+	resp, err := http.Get(url)
+	if err != nil {
+		return WeatherResponse{}, err
+	}
+	defer resp.Body.Close()
+
+	var apiResponse ExternalAPIResponse
+	if err := json.NewDecoder(resp.Body).Decode(&apiResponse); err != nil {
+		return WeatherResponse{}, err
+	}
+
+	return WeatherResponse{
+		City:        strings.Title(cityName),
+		Temperature: apiResponse.Temperature,
+		Weather:     apiResponse.Description,
+	}, nil
 }
