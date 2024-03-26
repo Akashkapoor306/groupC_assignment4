523b5328ed63c7c565fa5811f38ecfebb8a6dbac
AbhinavTIS <abhinavproam@gmail.com>
2024-03-26 18:11:39 -0400

    define ExternalAPIResponse struct

--- main.go
+++ main.go
@@ -1,7 +1,13 @@
 package main
 
-import "fmt"
-
-func main() {
-	fmt.Println("Hello Go Language")
+// ExternalAPIResponse defines the structure for the external weather API response
+type ExternalAPIResponse struct {
+	Temperature string `json:"temperature"`
+	Wind        string `json:"wind"`
+	Description string `json:"description"`
+	Forecast    struct {
+		Day         string `json:"day"`
+		Temperature string `json:"temperature"`
+		Wind        string `json:"wind"`
+	} `json:"forecast"`
 }
